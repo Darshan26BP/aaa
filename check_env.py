0457c546922d230cd7cdcc36ffd48a030c9d9db5
sanjaypoojary121 <sanjaypoojary121@gmail.com>
2025-11-10 15:27:49 +0530

    Initial commit

--- check_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
-print("OPENAI_PROJECT:", os.getenv("OPENAI_PROJECT"))
-print("OPENAI_ORG:", os.getenv("OPENAI_ORG"))
